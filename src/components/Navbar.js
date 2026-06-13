177a79de8e76d6e9999b0d8983c93e4666dcb8dd
TarekAbdo223 <tarekk.abdo22@gmail.com>
2026-06-13 15:47:49 +0300

    added the rel

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -57,7 +57,12 @@ const Navbar = () => {
             const { id, href, icon } = link;
             return (
               <li key={id}>
-                <a href={href} target="_blank" className="nav-icon">
+                <a
+                  rel="noreferrer"
+                  href={href}
+                  target="_blank"
+                  className="nav-icon"
+                >
                   <i className={icon}></i>
                 </a>
               </li>
